9fdf2dfa136390268744772add5464d80ff81078
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-10 14:11:30 +0800

    Parsing form bodies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('TEST');
+import express, { Request, Response } from 'express';
+
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3000, () => console.log('Listening on port 3000'));
