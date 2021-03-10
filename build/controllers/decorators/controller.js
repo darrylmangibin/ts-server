33761ec346038aedafe5eb82b2b622d905e445cb
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-10 17:48:14 +0800

    Add automated validation

--- build/controllers/decorators/controller.js
+++ build/controllers/decorators/controller.js
@@ -11,6 +11,22 @@ exports.controller = void 0;
 require("reflect-metadata");
 var appRouter_1 = require("../../appRouter");
 var MetadataKeys_1 = require("./MetadataKeys");
+function bodyValidators(keys) {
+    return function (req, res, next) {
+        if (!req.body) {
+            res.status(422).send('Invalid request');
+            return;
+        }
+        for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
+            var key = keys_1[_i];
+            if (!req.body[key]) {
+                res.status(422).send('Invalid request');
+                return;
+            }
+        }
+        next();
+    };
+}
 function controller(routePrefix) {
     return function (target) {
         var router = appRouter_1.AppRouter.getInstance();
@@ -20,8 +36,12 @@ function controller(routePrefix) {
             var method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
             var middlewares = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.middleware, target.prototype, key) ||
                 [];
+            var requiredBodyProps = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.validator, target.prototype, key) ||
+                [];
+            var validator = bodyValidators(requiredBodyProps);
             if (path) {
-                router[method].apply(router, __spreadArrays(["" + routePrefix + path], middlewares, [routehandler]));
+                router[method].apply(router, __spreadArrays(["" + routePrefix + path], middlewares, [validator,
+                    routehandler]));
             }
         }
     };
